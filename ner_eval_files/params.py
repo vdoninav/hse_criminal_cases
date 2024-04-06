5b84db9cd62979e78ce97568b87e55d29139ce43
Aleksei Vdonin <aleksei@vdonin.com>
2024-04-06 19:45:49 +0300

    try eval

--- ner_eval_files/params.py
+++ ner_eval_files/params.py
@@ -1,14 +1,14 @@
 import torch
 import evaluate
 
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('mps' if torch.cuda.is_available() else 'cpu')
 
 # IND [2] - Individual
 # LE [4] - Legal Entity
 # PEN [9] -Penalty
 # LAW [13] - Law
 # CR [17] - Crime
-LABEL_LIST=['B-CR', 'B-IND', 'B-LAW', 'B-LE', 'B-PEN', 'I-CR', 'I-IND', 'I-LAW', 'I-LE', 'I-PEN', 'O']
+LABEL_LIST = ['B-CR', 'B-IND', 'B-LAW', 'B-LE', 'B-PEN', 'I-CR', 'I-IND', 'I-LAW', 'I-LE', 'I-PEN', 'O']
 
 KEYS = ["start",
         "end",
@@ -16,11 +16,11 @@ KEYS = ["start",
         "entity_id",
         "entity_type"
         ]
-TYPES = {2:"IND",
-         4:"LE",
-         9:"PEN",
-         13:"LAW",
-         17:"CR",
+TYPES = {2: "IND",
+         4: "LE",
+         9: "PEN",
+         13: "LAW",
+         17: "CR",
          }
 
 TRAIN_PATH = '/content/drive/MyDrive/coursework2024/RuLegalNER/train.csv'
@@ -38,7 +38,8 @@ WEIGHT_DECAY = 0.01
 
 METRIC = evaluate.load("seqeval")
 
-CHECKPOINTS_DIR='/content/drive/MyDrive/coursework2024/checkpoints/'
+CHECKPOINTS_DIR = '/content/drive/MyDrive/coursework2024/checkpoints/'
 
 # MODEL_CKECKPOINT = "cointegrated/rubert-tiny"
-MODEL_CKECKPOINT = CHECKPOINTS_DIR+'best-checkpoint-yet'
+
+MODEL_CKECKPOINT = 'best-checkpoint-yet'
